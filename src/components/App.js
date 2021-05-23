b29b845782c6b15b74bb8e3f184593f5af80a656
Atif Khan <29atiffkhann@gmail.com>
2021-05-23 20:15:00 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,56 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-constructor(props){
-super(props)
-this.state = {
-    time: new Date()
-}
-this.timerId = null
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date(),
+    };
+    this.intervalId = null; 
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.getTimeString()}</h3>
+      </div>
+    );
+  }
 
-componentDidMount(){
-    this.timerId = setInterval(()=>{
-        this.setState({
-            time: new Date()
-        })
-    },1000)
-}
-componentWillUnmount(){
-    clearInterval(this.timerId)
-}
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        time: new Date(),
+      });
+    }, 1 * 1000);
+  }
 
-getTime(){
-const currentTime = this.state.time
-const [hours,minutes,seconds] = [currentTime.getHours(), currentTime.getMinutes(), currentTime.getSeconds()]
-const ampm = `${hours >=12 ? "PM" : "AM"}`  
+  componentWillUnmount() {
+      clearInterval(this.intervalId);
+  }
 
-const hour = hours> 12? hours-12 : hours
-const getHours = this.padNumber(hour) 
-const getMinutes = this.padNumber(minutes) 
-const getseconds = this.padNumber(seconds)
+  getTimeString() {
+    const currTime = this.state.time;
+    const [hours, minutes, seconds] = [
+      currTime.getHours(),
+      currTime.getMinutes(),
+      currTime.getSeconds(),
+    ];
 
-const timeString = `${getHours}:${getMinutes}:${getseconds} ${ampm}` 
-return timeString;
+    const amOrPm = hours >= 12 ? "PM" : "AM";
+    const twelveHourFormat = hours > 12 ? hours - 12 : hours;
+    const hourString = this.padNumberToTwoDigits(twelveHourFormat);
+    const minString = this.padNumberToTwoDigits(minutes);
+    const secondString = this.padNumberToTwoDigits(seconds);
+
+    const timeString = `${hourString}:${minString}:${secondString} ${amOrPm}`;
+    return timeString;
+  }
+
+  padNumberToTwoDigits(num) {
+   
+    return `${num < 10 ? "0" : ""}${num}`; 
+  }
 }
-padNumber(num){
-    return `${num < 10 ? "0" : ""}${num}`
-}
-    render() {
-
-        return(
-            <>
-            <div className="Clock">
-            <h3 id="time">{this.getTime()}</h3>
-            </div>
-               
-            </>
-        )
-    }
-}
-
 
 export default App;
